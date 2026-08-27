f898713ef86c8ec0a8a82ea0f9833564939919df
prabhatsin <prabhat.singh112244@gmail.com>
2026-08-28 03:19:47 +0530

    password hashing + jwt

--- core/security.py
+++ core/security.py
@@ -1,7 +1,8 @@
 # Password_Hashing
 
 import bcrypt
-
+from dotenv import load_dotenv
+load_dotenv()
 
 def hash_password(password):
     password_bytes=password.encode()
@@ -10,15 +11,6 @@ def hash_password(password):
     # decode it before storing inside the database 
 
 
-# pw='prabhat123'
-# result=hash_password(pw)
-# print(result)
-
-# if bcrypt.checkpw(pw.encode(),result):
-#     print("It matched")
-# else:
-#     print('Password did not match')
-
 #! small mistake
 '''
 hashed is bytes, but your DB column will likely be String. Store it 
@@ -26,18 +18,88 @@ as hashed.decode('utf-8')
 
 '''
 def verify_password(password,hashed_password):
-    # This is binary password
-    # !hashed_password=hash_password(password) 
+    # This is binary password, as hashed password from db is a string
     encoded_hash=hash_password.encode()
-    if bcrypt.checkpw(password.encode(),hashed_password):
-        # print("It matched")
+    if bcrypt.checkpw(password.encode(),encoded_hash):
         return {"message":"Its matched"}
     else:
         return{"message":"Password did not match"}
 
-pw='prabhat123'
 
-print(verify_password(pw))
+#TODO: Dependency to protect routes:
 
-# This hashed passowrd in above function is directly c alled from the db\
-# dont call the hashing function internally , 
+from jose import jwt
+from datetime import datetime ,timedelta,timezone
+
+import os 
+secret_key=os.environ.get("SECRET_KEY")
+
+def create_jwt_token(payload):
+
+    token=jwt.encode(payload,secret_key,algorithm='HS256')
+
+    return token
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#TODO: Explore the JWE part ,JSON Web Encryption
+'''
+JWT
+ │
+ ├── JWS → signing → integrity/authenticity
+ │
+ └── JWE → encryption → confidentiality
+
+'''
+
+
+
+#! Decoding experiment 
+
+# import base64
+
+# header = b"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+
+# decoded_header = base64.urlsafe_b64decode(header)
+
+# print(decoded_header.decode())
+
+
+
+
+
+
+
+# import base64
+
+# PAYLOADS = b"eyJ1c2VyX2lkIjoxMiwicm9sZSI6ImNsaWVudCIsImV4cCI6MTc4ODAwMDAwMH0"
+
+# padding = b"=" * (-len(PAYLOADS) % 4)
+
+# decoded = base64.urlsafe_b64decode(PAYLOADS + padding)
+
+# print(decoded.decode())
