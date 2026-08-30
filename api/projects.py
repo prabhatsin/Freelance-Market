1ec76f7d13c25f54ceff5b5bf13a7e5b363c0782
prabhatsin <prabhat.singh112244@gmail.com>
2026-08-30 23:56:42 +0530

    Project_List , using pultiple joins executed

--- api/projects.py
+++ api/projects.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 from db.database import get_db
 from fastapi import Depends,APIRouter
 from sqlalchemy.orm import Session
-from schema.schema import CreateProject,ProjectResponse
-from core.dependencies import get_current_client
-from models.models import Project 
+from sqlalchemy import select,func
+from schema.schema import CreateProject,ProjectResponse,ProjectListItem
+from core.dependencies import get_current_user,get_current_client
+from models.models import Project ,User,Proposals
 
 
 
@@ -32,16 +33,151 @@ def create_projects(project_request:CreateProject,
     return new_project
 
 
+'''
+    id:int
+    project_title: str
+    project_description: str
+    category: str
+    budget_min: PositiveFloat
+    budget_max: PositiveFloat
+    deadline: date
+    status: ProjectStatus
+    client_name:str
+    proposal_count:int
 
-@router.get("/projects")
-def list_projects(db:Session=Depends(get_db)):
-    pass
+
+'''
+
+
+# Here we have to perfrom join operation in order to get client_name , proposal_count
+
+@router.get("/projects",response_model=list[ProjectListItem])
+def list_projects(current_user=Depends(get_current_user),db:Session=Depends(get_db)):
+    # Authenticated user meaning , get_current user only (via jwt verification)
+    # It shouldnt be client/freelancer based anyone can see it
+    statement=(
+        select(Project.id,
+               Project.project_title,
+               Project.project_description,
+               Project.category,
+               Project.budget_min,
+               Project.budget_max,
+               Project.deadline,
+               Project.status,
+               User.name.label("client_name"),
+               func.count(Proposals.id).label("proposal_count") 
+               )
+        .join(User,Project.client_id==User.id)
+        .outerjoin(Proposals,Project.id==Proposals.project_id)
+        .where(Project.status=='OPEN')
+        .group_by(Project.id,User.name)
+               )
+        
+    print(statement)
+    result=db.execute(statement).all()
+    return result
+
+
+
+
+#! LEARNING
+
+#--------------------------------------------------------------------------------------------------
+#? IMPORTANT NOTE:
+# db.query , This the legacy style of querying the db , described in legacy document (v1) 
+# modern style is , described in version 2 
+
+'''
+stmt = (
+    select(User)
+    .join(User.addresses)
+)
+
+'''
+'''
+SQLAlchemy 2.x has standardized around:
+
+select()
+   ↓
+db.execute()
+   ↓
+Result
+------------------
+The older:
+
+db.query()
+
+'''
+
+'''
+Joins in ORM two famous styles 
+1. defined relationship in tyhe models 
+2. explicitly use on , just like SQl queries 
+
+
+'''
+#Why .join at one place and .Outerjoint at other 
+
+'''
+1. join here means inner join ,
+
+It means:
+"Only return a project if its client exists."
+
+2. Outerjoin (Same left join in mysql queries)
+
+ "Return the project even if there is no matching proposal."
+
+'''
+
+
+
+#-----------------------------------------------------------------------------------------------------
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+#! Question: We didnt needed ,"ProjectResponse.model_validate(new_project)" in our first api observe ??
+
+'''
+Your current flow(the first api)
+
+
+Database
+   ↓
+SQLAlchemy Project object
+   ↓
+return new_project
+   ↓
+FastAPI sees response_model=ProjectResponse
+   ↓
+Pydantic converts/validates it
+   ↓
+JSON response
+
+#!When would you actually use model_validate() yourself?
+
+Suppose you're doing something inside your Python code, not simply returning the object from a FastAPI endpoint:
+
+You need model_validate() manually when you want to explicitly convert a SQLAlchemy/other object into a Pydantic
+model inside your Python code,(i.e u need the json .dict format in ur code) before returning or passing it elsewhere.
+
+If you're simply returning the ORM object from a FastAPI endpoint with response_model=..., FastAPI performs 
+this conversion for you, so manual model_validate() isn't needed.
+
+'''
 
 
 
