30ae60432a4bf866613fbc717a47959677117b71
prabhatsin <prabhat.singh112244@gmail.com>
2026-09-03 10:58:04 +0530

    CORS updated

--- api/main.py
+++ api/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://freelance-market-frontend.vercel.app",
-        "https://freelance-market.prabhatsingh.dev",
+        "https://freelancemarket.prabhatsingh.dev",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
